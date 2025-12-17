dd55a1e1ddba27c78f42e91ee9cf872b4e528147
빵돌 <schicksal36@naver.com>
2025-12-17 10:38:18 +0900

    Delete 책영화평론_Flask directory

--- 책영화평론_Flask/app/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-from datetime import datetime
-
-class Review(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
