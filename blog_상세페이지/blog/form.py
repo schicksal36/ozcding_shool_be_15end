9281a5079666bb26e5857eed9f60a7f9655668ce
빵돌 <schicksal36@naver.com>
2026-01-28 16:41:39 +0900

    Delete blog_상세페이지 directory

--- blog_상세페이지/blog/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Blog
-
-class BlogForm(forms.ModelForm) : 
-    class Meta : 
-        model = Blog
-        #fields = __all__ 전체 적용시
-        fields = ('title' ,'content')
